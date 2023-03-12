204da47fa8a0d2d21df8aa786d22e12d5183e8a4
oleg <legik7127@gmail.com>
2023-03-12 10:42:36 +0600

    added method find(Node node, int value)

--- src/main/java/Tree.java
+++ src/main/java/Tree.java
@@ -1,2 +1,25 @@
+import java.util.List;
+
 public class Tree {
+    Node root;
+
+    private Node find(Node node, int value){
+        if (node.value == value){
+            return node;
+        }
+        else {
+            for (Node child : node.children) {
+                Node result = find(child, value);
+                if (result != null){
+                    return result;
+                }
+            }
+        }
+        return null;
+    }
+
+    public class Node{
+        int value;
+        List<Node> children;
+    }
 }
