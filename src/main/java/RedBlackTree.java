a8579f04f7297ee9ab238527f97a0a69b8a426f2
oleg <legik7127@gmail.com>
2023-03-13 21:53:35 +0600

    added method rebalance

--- src/main/java/RedBlackTree.java
+++ src/main/java/RedBlackTree.java
@@ -1,5 +1,3 @@
-import java.awt.*;
-
 public class RedBlackTree {
     private Node root;
 
@@ -22,17 +20,81 @@ public class RedBlackTree {
             return false;
         }
         else {
-            if (node.value > value){
-                if (node.leftChild != null){
+            if (node.value > value) {
+                if (node.leftChild != null) {
                     boolean result = addNode(node.leftChild, value);
                     node.leftChild = rebalance(node.leftChild);
                     return result;
-                }
-                else{
+                } else {
                     node.leftChild = new Node();
-
+                    node.leftChild.color = Color.RED;
+                    node.leftChild.value = value;
+                    return true;
                 }
             }
+            else {
+                if (node.leftChild != null) {
+                    boolean result = addNode(node.leftChild, value);
+                    node.leftChild = rebalance(node.leftChild);
+                    return result;
+                } else {
+                    node.leftChild = new Node();
+                    node.leftChild.color = Color.RED;
+                    node.leftChild.value = value;
+                    return true;
+                }
+
+            }
         }
+    }
+
+    private Node rebalance(Node node){
+        Node result = node;
+        boolean needRebalance;
+        do {
+            needRebalance = false;
+            if (result.rightChild != null && result.rightChild.color == Color.RED &&
+                    (result.leftChild == null || result.leftChild.color == Color.BLACK)){
+                needRebalance = true;
+                result = rightSwap(result);
+            }
+            if (result.leftChild != null && result.leftChild.color == Color.RED &&
+                    (result.leftChild.leftChild != null && result.leftChild.leftChild.color == Color.RED)){
+                needRebalance = true;
+                result = leftSwap(result);
+            }
+            if (result.leftChild != null && result.leftChild.color == Color.RED &&
+                    (result.rightChild != null && result.rightChild.color == Color.RED)){
+                needRebalance = true;
+                colorSwap(result);
+            }
+
+        }
+    }
+
+    private Node rightSwap(Node node){
+        Node rightChild = node.rightChild;
+        Node betweenChild = rightChild.leftChild;
+        rightChild.leftChild = node;
+        node.rightChild = betweenChild;
+        rightChild.color = node.color;
+        node.color = Color.RED;
+        return rightChild;
+    }
+
+    private Node leftSwap(Node node){
+        Node leftChild = node.leftChild;
+        Node betweenChild = leftChild.leftChild;
+        leftChild.leftChild = node;
+        node.leftChild = betweenChild;
+        leftChild.color = node.color;
+        node.color = Color.RED;
+        return leftChild;
+    }
+
+    private void colorSwap(Node node){
+        node.leftChild.color = Color.BLACK;
+        node.leftChild.color = Color.BLACK;
+        node.color = Color.RED;
     }
 }
